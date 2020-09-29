be3dc296b8af253a9f4b8bf150465feac3558f6e
Will Jukes <william.r.jukes@gmail.com>
2020-09-29 14:55:47 -0400

    remove rogue django package

--- habitapp/models.py
+++ habitapp/models.py
@@ -5,3 +5,5 @@ class Habits(models.Model):
     goal = models.CharField(max_length=255, null=False, blank=False)
     date = models.DateTimeField(auto_now_add=True)
     integer = models.IntegerField(null=False, blank=False)
+
+
