ebc5bcef1e84e2f71aaa8da012c56abf7a0e8257
Harrison Ballard <harrisonwballard@gmail.com>
2020-09-29 20:52:16 -0400

    added record model, minor html tweaks, some commenting, and fixing bugs

--- habitapp/models.py
+++ habitapp/models.py
@@ -5,6 +5,11 @@ class Habit(models.Model):
     goal = models.CharField(max_length=255, null=False, blank=False)
     date = models.DateTimeField(auto_now_add=True)
    
+class HabitRecord(models.Model):
+    habit = models.ForeignKey(Habit, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+
+
 
     def __str__(self):
         return f"{self.goal}"
